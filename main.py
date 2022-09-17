82baea34ba6680bda90d17a02501ac18533256fe
Andrey <68823111+Corochco@users.noreply.github.com>
2022-09-18 00:09:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -101,8 +101,127 @@ def show_contacts():
             line = f.readline()
         return array
 
+def find_by_phone_number(contacts_array):
+    b = False
+    command = ' '.join(input().split())
+    for i in contacts_array:
+        if str(i.get_phone_number()) == str(command):
+            print(i.pyout())
+            b = True
+            line = i
+            break
+    if not b:
+        print('Извините, но по вашему запросу контактов не найдено')
+        return 0
+    return line
+
+def find_by_email(contacts_array):
+    b = False
+    command = ' '.join(input().split())
+    for i in contacts_array:
+        if str(i.get_email()) == str(command):
+            print(i.pyout())
+            b = True
+            line = i
+            break
+    if not b:
+        print('Извините, но по вашему запросу контактов не найдено')
+        return 0
+    return line
+
+def find_by_name(contacts_array):
+    b = False
+    command = ' '.join(input().split())
+    for i in contacts_array:
+        if str(i.get_name()) == str(command):
+            print(i.pyout())
+            b = True
+    if not b:
+        print('Извините, но по вашему запросу контактов не найдено')
+
+def find_by_surname(contacts_array):
+    b = False
+    command = ' '.join(input().split())
+    for i in contacts_array:
+        if str(i.get_surname()) == str(command):
+            print(i.pyout())
+            b = True
+            line = i
+    if not b:
+        print('Извините, но по вашему запросу контактов не найдено')
+        return 0
+    return line
+
+def find_by_middle_name(contacts_array):
+    b = False
+    command = ' '.join(input().split())
+    for i in contacts_array:
+        if str(i.get_middle_name()) == str(command):
+            print(i.pyout())
+            b = True
+    if not b:
+        print('Извините, но по вашему запросу контактов не найдено')
+
+def find_by_surname_and_name(contacts_array):
+    b = False
+    surname, name = map(str, input().split())
+    for i in contacts_array:
+        if str(i.get_surname()) == str(surname) and str(i.get_name()) == str(name):
+            print(i.pyout())
+            b = True
+            line = i
+    if not b:
+        print('Извините, но по вашему запросу контактов не найдено')
+        return 0
+    return line
+
+def find_by_all(contacts_array):
+    b = False
+    surname, name, middle_name = map(str, input().split())
+    for i in contacts_array:
+        if str(i.get_surname()) == str(surname) and str(i.get_name()) == str(name) and str(i.get_middle_name()) == str(middle_name):
+            print(i.pyout())
+            b = True
+            line = i
+    if not b:
+        print('Извините, но по вашему запросу контактов не найдено')
+        return 0
+    return line
+
+def find_empty_phone_number(contacts_array):
+    b = False
+    for i in contacts_array:
+        if str(i.get_phone_number()) == '':
+            print(i.pyout())
+            b = True
+    if not b:
+        print('У всех контактов заполнены номера телефонов')
+
+def find_empty_email(contacts_array):
+    b = False
+    for i in contacts_array:
+        if str(i.get_email()) == '':
+            print(i.pyout())
+            b = True
+    if not b:
+        print('У всех контактов заполнены номера телефонов')
+
+def find_empty_email_and_phone_number(contacts_array):
+    b = False
+    for i in contacts_array:
+        if str(i.get_email()) == '' and str(i.get_phone_number()) == '':
+            print(i.pyout())
+            b = True
+    if not b:
+        print('У всех контактов заполнены номера телефонов')
+
+def re_write(contacts_array):
+    with open('contacts.txt', 'w') as f:
+        for i in contacts_array:
+            f.write(i.get_surname() + ' ' + i.get_name() + ' ' + i.get_middle_name() + ', ' + i.get_phone_number() + ', ' + i.get_email() + '\n')
+
 #приветственное сообщение и первая команда(в приведственном сообщении распишем все команды
-print('Введите команду (если хотите закрыть приложение, введите close. \nДля вывода всех контактов введите show contacts. \nДля поиска по номеру телефона введите find by phone number. \nДля поиска по электронной почте введите find by email. \nДля поиска по имени введите find by name. \nДля поиска по фамилии введите find by surname. \nДля поиска по отчеству введите find by middle name. \nДля поиска по фамилии, имени, отчеству введите find by all (пожалуйста пишите фамилию, имя и отчество в порядке, как в этой строке). \nДля поиска контактов без номера телефона введите find empty phone number. \nДля поиска контактов без номера телефона введите find empty email. \nДля поиска контактов без номера телефона и почты введите find empty email and phone number.')
+print('Введите команду (если хотите закрыть приложение, введите close. \nДля вывода всех контактов введите show contacts. \nДля поиска по номеру телефона введите find by phone number. \nДля поиска по электронной почте введите find by email. \nДля поиска по имени введите find by name. \nДля поиска по фамилии введите find by surname. \nДля поиска по отчеству введите find by middle name. \nДля поиска по фамилии, имени, отчеству введите find by all (пожалуйста пишите фамилию, имя и отчество в порядке, как в этой строке).\nДля поиска по фамилии и имени введите find by surname and name (пожалуйста пишите фамилию и имя в порядке, как в этой строке).  \nДля поиска контактов без номера телефона введите find empty phone number. \nДля поиска контактов без номера телефона введите find empty email. \nДля поиска контактов без номера телефона и почты введите find empty email and phone number. \nДля редактирования контакта введите change contact.')
 command = ' '.join(input().split())
 
 #"сердце" программы
@@ -113,87 +232,79 @@ while command != 'close':
         for i in contacts_array:
             print(i.pyout())
     elif command == 'find by phone number':
-        b = False
-        command = ' '.join(input().split())
-        for i in contacts_array:
-            if str(i.get_phone_number()) == str(command):
-                print(i.pyout())
-                b = True
-                break
-        if not b:
-            print('Извините, но по вашему запросу контактов не найдено')
+        find_by_phone_number(contacts_array)
     elif command == 'find by email':
-        b = False
-        command = ' '.join(input().split())
-        for i in contacts_array:
-            if str(i.get_email()) == str(command):
-                print(i.pyout())
-                b = True
-                break
-        if not b:
-            print('Извините, но по вашему запросу контактов не найдено')
+        find_by_email(contacts_array)
     elif command == 'find by name':
-        b = False
-        command = ' '.join(input().split())
-        for i in contacts_array:
-            if str(i.get_name()) == str(command):
-                print(i.pyout())
-                b = True
-        if not b:
-            print('Извините, но по вашему запросу контактов не найдено')
+        find_by_name(contacts_array)
     elif command == 'find by surname':
-        b = False
-        command = ' '.join(input().split())
-        for i in contacts_array:
-            if str(i.get_surname()) == str(command):
-                print(i.pyout())
-                b = True
-        if not b:
-            print('Извините, но по вашему запросу контактов не найдено')
+        find_by_surname(contacts_array)
     elif command == 'find by middle name':
-        b = False
-        command = ' '.join(input().split())
-        for i in contacts_array:
-            if str(i.get_middle_name()) == str(command):
-                print(i.pyout())
-                b = True
-        if not b:
-            print('Извините, но по вашему запросу контактов не найдено')
+        find_by_middle_name(contacts_array)
+    elif command == 'find by surname and name':
+        find_by_surname_and_name(contacts_array)
     elif command == 'find by all':
-        b = False
-        surname, name, middle_name = map(str, input().split())
-        for i in contacts_array:
-            if str(i.get_surname()) == str(surname) and str(i.get_name()) == str(name) and str(i.get_middle_name()) == str(middle_name):
-                print(i.pyout())
-                b = True
-        if not b:
-            print('Извините, но по вашему запросу контактов не найдено')
+        find_by_all(contacts_array)
     elif command == 'find empty phone number':
-        b = False
-        for i in contacts_array:
-            if str(i.get_phone_number()) == '':
-                print(i.pyout())
-                b = True
-        if not b:
-            print('У всех контактов заполнены номера телефонов')
+        find_empty_phone_number(contacts_array)
     elif command == 'find empty email':
-        b = False
-        for i in contacts_array:
-            if str(i.get_email()) == '':
-                print(i.pyout())
-                b = True
-        if not b:
-            print('У всех контактов заполнены номера телефонов')
+        find_empty_email(contacts_array)
     elif command == 'find empty email and phone number':
-        b = False
-        for i in contacts_array:
-            if str(i.get_email()) == '' and str(i.get_phone_number()) == '':
-                print(i.pyout())
-                b = True
-        if not b:
-            print('У всех контактов заполнены номера телефонов')
+        find_empty_email_and_phone_number(contacts_array)
+    elif command == 'change contact':
+        print('Для изменения контакта выберите способ его нахождения \n(ниже выберете в зависимости от всех атрибутов контакта, то есть если есть только фамилия, то поиск по фамилии и тп)\nПоиск по фамилии find by surname \nПоиск по фамилии и имени find by surname and name \nПоиск по фамилии, имени и отчеству find by all \nПоиск по номеру телефона find by phone number\nПоиск по электронной почте find by email.')
+        command = ' '.join(input().split())
+        if command == 'find by phone number':
+            line = find_by_phone_number(contacts_array)
+        elif command == 'find by email':
+            line = find_by_email(contacts_array)
+        elif command == 'find by surname':
+            line = find_by_surname(contacts_array)
+        elif command == 'find by surname and name':
+            line = find_by_surname_and_name(contacts_array)
+        elif command == 'find by all':
+            line = find_by_all(contacts_array)
+        else:
+            print('Я не понимаю вашу команду, начните с начала, пожалуйста')
+        print('Как вы хотите изменить этот контакт?\nЕсли хотите изменить номер, напишите change number.\nЕсли хотите изменить почту, напишите change email.\nЕсли хотите изменить фамилию, напишите change surname.\nЕсли хотите изменить имя, напишите change name.\nЕсли хотите изменить отчество, напишите change middle name.')
+        command = ' '.join(input().split())
+        if str(command) == 'change number':
+            print('Введите новый номер')
+            command = ' '.join(input().split())
+            line2 = Contact(line.surname, line.name, line.middle_name, command, line.email)
+            contacts_array[contacts_array.index(line)] = line2
+            re_write(contacts_array)
+            print('Изменения успешно внесены!')
+        elif str(command) == 'change email':
+            print('Введите новую почту')
+            command = ' '.join(input().split())
+            line2 = Contact(line.surname, line.name, line.middle_name, line.phone_number, command)
+            contacts_array[contacts_array.index(line)] = line2
+            re_write(contacts_array)
+            print('Изменения успешно внесены!')
+        elif str(command) == 'change name':
+            print('Введите новое имя')
+            command = ' '.join(input().split())
+            line2 = Contact(line.surname, command, line.middle_name, line.phone_number, line.email)
+            contacts_array[contacts_array.index(line)] = line2
+            re_write(contacts_array)
+            print('Изменения успешно внесены!')
+        elif str(command) == 'change surname':
+            print('Введите новую фамилию')
+            command = ' '.join(input().split())
+            line2 = Contact(command, line.name, line.middle_name, line.phone_number, line.email)
+            contacts_array[contacts_array.index(line)] = line2
+            re_write(contacts_array)
+            print('Изменения успешно внесены!')
+        elif str(command) == 'change middle name':
+            print('Введите новое отчество')
+            command = ' '.join(input().split())
+            line2 = Contact(line.surname, line.name, command, line.phone_number, line.email)
+            contacts_array[contacts_array.index(line)] = line2
+            re_write(contacts_array)
+            print('Изменения успешно внесены!')
     else:
-        print('Извините, но я Вас не понимаю7')
+        print('Извините, но я Вас не понимаю :(')
 
     command = ' '.join(input().split())
 
